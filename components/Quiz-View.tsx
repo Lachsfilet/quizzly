45873f81733afc3f28742d3511ddcc2d09c6e607
Fynn Berger <99832883+Lachsfilet@users.noreply.github.com>
2024-06-05 09:19:15 +0200

    UPDATE QUIZ view

--- components/Quiz-View.tsx
+++ components/Quiz-View.tsx
@@ -7,6 +7,7 @@ import toast from 'react-hot-toast'
 import { MouseEventHandler } from 'react'
 import { Question } from '@/interfaces/question'
 import { useRouter } from 'next/navigation'
+import { Badge } from '@/components/ui/badge'
 
 interface QuizViewProps {
   question: Question
@@ -40,31 +41,30 @@ export function QuizView({
 
   return (
     <div className="w-full mt-20">
-      <div className="w-full flex ml-5 h-auto">
-        {!session.image ? (
-          <h1 className="text-xl ml-2.5 self-center">{session.name}</h1>
-        ) : (
-          <>
-            <Avatar
-              src={session.image}
-              alt="user image"
-              className="rounded-full"
-            />
-            <h1 className="text-xl ml-2.5 self-center">{session.name}</h1>
-            <h1 className="text-xl self-center">{score}</h1>
-          </>
-        )}
+      <div className="w-full flex justify-between items-center ml-5 mr-16 h-auto">
+        <div className="flex items-center">
+          <Avatar
+            src={session.image ? session.image : ''}
+            alt="logo"
+            className="mr-5 shadow-md shadow-black hover:scale-110 transition-transform duration-300  bg-gradient-to-r from-gradient-start to-gradient-end"
+            sx={{ width: 40, height: 40 }}
+          ></Avatar>
+          <h1 className="text-xl">{session.name}</h1>
+        </div>
+        <Badge variant={'secondary'} className="w-20 h-10 mr-10 text-center">
+          <h2 className="text-xl">{score}</h2>
+        </Badge>
       </div>
-      <div className="w-full">
-        <div>
-          <h1 className="text-5xl flex items-center justify-center backdrop-blur bg-accent/25">
-            {question.title}
-          </h1>
+      <div className="w-full mt-8">
+        <h1 className="text-5xl flex items-center justify-center backdrop-blur bg-accent/25">
+          {question.title}
+        </h1>
+        <div className="mt-8 flex flex-col items-center">
           {optionsArray.map((option: Option, i: number) => (
             <Button
               key={i}
               onClick={validateQuiz(option.isCorrect)}
-              className="text-5xl"
+              className="text-2xl mb-4"
             >
               {option.title}
             </Button>
